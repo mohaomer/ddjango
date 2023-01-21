2479784d656f491989f62d50e8f911ce8f3d6a41
mohaomer <mohaomer57@gmail.com>
2023-01-21 22:52:16 +0200

    add css,img and html files and create model ,views, urls files

--- project/settings.py
+++ project/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'restaurant.apps.Restaurant.Config',
+    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "restaurant",
 ]
 
 MIDDLEWARE = [
